337208f68a5bde0d0aef9a9df5a183bbdc5514e3
Kris Hopper <hopperdavis@gmail.com>
2021-06-18 08:58:05 -0700

    pass function NewPizza() test

---
+++ README.md
@@ -0,0 +1,9 @@
+Describe: function NewPizza()
+
+Test: It will create a new object "newPizza" with no id.
+Code: let newPizza = new NewPizza()
+Expected Result: newPizza; undefined currentId: 0, pizza: {}
+
+Describe: function AssignId()
+
+Test: It will assign an idea to object "newPizza".
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Pizza constructor with this.s for toppings (embed them?) drop down menus for different options. Radio buttons (select multiple) for different toppings - insert into constructor for pizza. Radio button for size (select one). constructor for new pizza, constructor for pizza toppings/size, method for pizza price.
+
+//Business Logic
+function NewPizza() {
+  this.pizza = {}
+  this.currentId = 0
+}
+
+
+
+
+
+
+//UI Logic
