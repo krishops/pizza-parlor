9c19a704a0da8dc4b1f9b618da53f4e40a4765d0
Kris Hopper <hopperdavis@gmail.com>
2021-06-18 15:08:00 -0700

    WIP still trying to make price caluclating method

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,26 @@ NewParlor.prototype.findPizza = function(id) {
   return false;
 };
 
-NewParlor.prototype.calculatePrice = function(size, one, two, three, four) {
-  this.pizzaPrice = size + one + two + three + four
-  console.log(this.pizzaPrice)
+NewParlor.prototype.totalPrice = function() {
+  this.pizzaPrice = 0
+
+NewParlor.prototype.calculatePrice = function() {
+  switch (this.findPizza())  {
+        case (this.size === "small"):
+          this.pizzaPrice += 5;
+          break;
+        case ("medium"):
+          this.pizzaPrice += 7;
+          break;
+        case ("large"):
+          this.pizzaPrice += 9;
+          break;
+      
 }
+return pizzaPrice;
+
+}
+console.log(this.pizzaPrice)
 // NewParlor.prototype.pizzaPrice = function() {
 //   this.pizzaPrice = (this.pizzaSize) + parseInt(this.pizza[id].toppingOne) + parseInt(this.pizza[id].toppingTwo) + parseInt(this.pizza[id].toppingThree) + parseInt(this.pizza[id].toppingFour)
 // }
@@ -38,8 +54,8 @@ NewParlor.prototype.calculatePrice = function(size, one, two, three, four) {
 // }
 
 // Business Logic for Pizza
-function NewPizza(Size, toppingOne, toppingTwo, toppingThree, toppingFour) {
-  this.size = Size;
+function NewPizza(size, toppingOne, toppingTwo, toppingThree, toppingFour) {
+  this.size = size;
   this.toppingOne = toppingOne;
   this.toppingTwo = toppingTwo;
   this.toppingThree = toppingThree;
@@ -57,8 +73,8 @@ function NewPizza(Size, toppingOne, toppingTwo, toppingThree, toppingFour) {
 //UI Logic
 let newParlor = new NewParlor();
 
-$(document).ready(function()  {
-  $("form#pizza-parlor").submit(function(event) {
+$(document).ready(function () {
+  $("form#pizza-parlor").submit(function (event) {
     event.preventDefault();
     const inputtedSize = $("input:radio[name=select-size]:checked").val();
     const inputtedToppingOne = $("#topping-one").val();
@@ -68,24 +84,25 @@ $(document).ready(function()  {
     let newPizza = new NewPizza(inputtedSize, inputtedToppingOne, inputtedToppingTwo, inputtedToppingThree, inputtedToppingFour)
     newParlor.addPizza(newPizza);
     console.log(newPizza)
+    // newParlor.calculatePrice(inputtedSize, inputtedToppingOne, inputtedToppingTwo, inputtedToppingThree, inputtedToppingFour)
     $(".pizza-size").html(inputtedSize);
     $(".first-topping").html(inputtedToppingOne);
     $(".second-topping").html(inputtedToppingTwo);
     $(".third-topping").html(inputtedToppingThree);
     $(".fourth-topping").html(inputtedToppingFour);
-  
-  if (inputtedSize === "small") {
-    sizePrice = 5;
-  } else if (inputtedSize === "medium") {
-    sizePrice = 7;
-  } else 
-    sizePrice = 9; 
 
-    newParlor.calculatePrice(sizePrice, 0, 0, 0, 0)
+    
+      // if (inputtedSize === "small") {
+      //   return 5;
+      // } else if (inputtedSize === "medium") {
+      //   return 7;
+      // } else if (inputtedSize === "large") {
+      //   return 9;}     
+      //   newParlor.calculatePrice(inputtedSize, 0, 0, 0, 0) 
+    });
+  });
 
-  }
-);
-});
+
 
 // Pizza constructor with this.s for toppings (embed them?) drop down menus for different options. Drop-down select forms for different toppings - insert into constructor for pizza? Radio button for size (select one). constructor for new pizza, constructor for pizza toppings/size, method for pizza price.
 //maybe assign prices to each key/key value ~ if topping One $2, if toppingTwo $4, can only select second topping if select first topping, etc. although I'm not sure if that would be in UI or HTML. Assign value in HTML? Similar to language selector project
@@ -115,7 +132,7 @@ $(document).ready(function()  {
 //   $(".second-topping").html(pizza.toppingTwo);
 //   $(".third-topping").html(pizza.toppingThree);
 //   $(".fourth-topping").html(pizza.toppingFour);
-  
+
   // let buttons = $("#buttons");
   // buttons.empty();
   // buttons.append("<button class ='deleteButton' id=" + + contact.id + ">Delete</button>");
