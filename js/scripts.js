c92784f5428b3de9a2abc616c5d9289576c2be4d
Kris Hopper <hopperdavis@gmail.com>
2021-06-18 14:20:41 -0700

    WIP working on calculating pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-// Pizza constructor with this.s for toppings (embed them?) drop down menus for different options. Drop-down select forms for different toppings - insert into constructor for pizza? Radio button for size (select one). constructor for new pizza, constructor for pizza toppings/size, method for pizza price.
-//maybe assign prices to each key/key value ~ if topping One $2, if toppingTwo $4, can only select second topping if select first topping, etc. although I'm not sure if that would be in UI or HTML. Assign value in HTML? Similar to language selector project
-
 //Business Logic for Parlor
 function NewParlor() {
   this.pizza = {}
   this.currentId = 0
+  this.pizzaPrice = 0
 }
 
 NewParlor.prototype.addPizza = function(pizza) {
@@ -24,6 +22,21 @@ NewParlor.prototype.findPizza = function(id) {
   return false;
 };
 
+NewParlor.prototype.calculatePrice = function(size, one, two, three, four) {
+  this.pizzaPrice = size + one + two + three + four
+  console.log(this.pizzaPrice)
+}
+// NewParlor.prototype.pizzaPrice = function() {
+//   this.pizzaPrice = (this.pizzaSize) + parseInt(this.pizza[id].toppingOne) + parseInt(this.pizza[id].toppingTwo) + parseInt(this.pizza[id].toppingThree) + parseInt(this.pizza[id].toppingFour)
+// }
+
+
+// NewParlor.prototype.itemPrices = function() {
+
+// function pizzaPrice() {
+//   if (inputtedSize === "small")
+// }
+
 // Business Logic for Pizza
 function NewPizza(Size, toppingOne, toppingTwo, toppingThree, toppingFour) {
   this.size = Size;
@@ -33,34 +46,16 @@ function NewPizza(Size, toppingOne, toppingTwo, toppingThree, toppingFour) {
   this.toppingFour = toppingFour;  //Possibly refactor this into nested objects
 }
 
-NewParlor.prototype.pizzaPrice = function() {
-  this.pizzaPrice = (this.pizzaSize) + parseInt(this.pizza[id].toppingOne) + parseInt(this.pizza[id].toppingTwo) + parseInt(this.pizza[id].toppingThree) + parseInt(this.pizza[id].toppingFour)
-}
-
-NewParlor.prototype.itemPrices = function() {
-  switch (this.size)  {
-    case ("small"):
-    this.pizzaSize += 5
-  }
-}
+// NewPizza.prototype.findPrice = function() {
+//   this.pizzaPrice = 0
+//   if (this.size === "small")  {
+//     this.pizzaPrice += 5;
+//   }
+//   console.log("HUZZAH", this.pizzaPrice)
+// }
 
 //UI Logic
 let newParlor = new NewParlor();
-
-// function showPizza(pizzaId) {
-//   const pizza = newParlor.findPizza(pizzaId);
-//   // $("#show-pizza").show();
-//   $(".pizza-size").html(pizza.size);
-//   $(".first-topping").html(pizza.toppingOne);
-//   $(".second-topping").html(pizza.toppingTwo);
-//   $(".third-topping").html(pizza.toppingThree);
-//   $(".fourth-topping").html(pizza.toppingFour);
-  
-  // let buttons = $("#buttons");
-  // buttons.empty();
-  // buttons.append("<button class ='deleteButton' id=" + + contact.id + ">Delete</button>");
-// }
-
 
 $(document).ready(function()  {
   $("form#pizza-parlor").submit(function(event) {
@@ -78,20 +73,50 @@ $(document).ready(function()  {
     $(".second-topping").html(inputtedToppingTwo);
     $(".third-topping").html(inputtedToppingThree);
     $(".fourth-topping").html(inputtedToppingFour);
-  })
-})
+  
+  if (inputtedSize === "small") {
+    sizePrice = 5;
+  } else if (inputtedSize === "medium") {
+    sizePrice = 7;
+  } else 
+    sizePrice = 9; 
+
+    newParlor.calculatePrice(sizePrice, 0, 0, 0, 0)
+
+  }
+);
+});
+
+// Pizza constructor with this.s for toppings (embed them?) drop down menus for different options. Drop-down select forms for different toppings - insert into constructor for pizza? Radio button for size (select one). constructor for new pizza, constructor for pizza toppings/size, method for pizza price.
+//maybe assign prices to each key/key value ~ if topping One $2, if toppingTwo $4, can only select second topping if select first topping, etc. although I'm not sure if that would be in UI or HTML. Assign value in HTML? Similar to language selector project
 
 
-const color = "red";
-switch (color) {
-  case ("red"):
-    console.log("Red!");
-  case ("green"):
-    console.log("Green!");
-    break;
-  case ("blue"):
-    console.log("Blue!");
-    break;
-  default:
-    console.log("It's not red, blue, or green.");
-}
+
+// NewParlor.prototype.itemPrices = function() {
+//   switch (this.pizzaPrice())  {
+//     case ("small"):
+//       this.pizzaPrice += 5;
+//       break;
+//     case ("medium"):
+//       this.pizzaPrice += 7;
+//       break;
+//     case ("large"):
+//       this.pizzaPrice += 9;
+//       break;
+//   }
+// }
+
+
+// function showPizza(pizzaId) {
+//   const pizza = newParlor.findPizza(pizzaId);
+//   // $("#show-pizza").show();
+//   $(".pizza-size").html(pizza.size);
+//   $(".first-topping").html(pizza.toppingOne);
+//   $(".second-topping").html(pizza.toppingTwo);
+//   $(".third-topping").html(pizza.toppingThree);
+//   $(".fourth-topping").html(pizza.toppingFour);
+  
+  // let buttons = $("#buttons");
+  // buttons.empty();
+  // buttons.append("<button class ='deleteButton' id=" + + contact.id + ">Delete</button>");
+// 
